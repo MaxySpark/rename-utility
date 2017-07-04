7e1dfb20acfca68af16df5bf520c749a6bda6b3d
MaxySpark <hello@maxyspark.com>
2017-07-05 00:11:36 +0530

    making async

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
+#!/usr/bin/env node
 'use strict'
 
 const fs = require("fs");
 const path = require("path");
+
+var dir = process.cwd();
+
+var fileList = [];
+
+fs.readdir(dir,(err,files)=>{
+    files.forEach((file)=>{
+        fileList.push(file);
+    });
+    console.log(fileList);
+});
